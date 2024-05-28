008d375852d7ec7a36c1732a3b8cab31f0989779
ummetarin <ummehomairatarin@gmail.com>
2024-05-28 20:24:34 +0600

    implement false nethod

--- Falsemethod.c
+++ Falsemethod.c
@@ -21,6 +21,25 @@ int main(){
         printf("X2 = ");
         scanf("%f",&x2);
     } while (F(x2)>0);
+
+    do
+    {
+        f1=F(x1);
+        f2=F(x2);
+
+        x0=(x1+x2)/2;
+        f0=F(x0);
+
+        if(f0*f1<0){
+            x2=x0;
+        }
+        else{
+            x1=x0;
+        }
+     
+    } while (fabs(f0) > E);
+    
+    
     
     
  
